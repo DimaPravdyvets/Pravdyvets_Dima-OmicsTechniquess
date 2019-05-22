e2a1118ab1aa4fce7f13566a94f09fac8b9c91a1
dima <dmytro.pravdyvets@alum.esci.upf.edu>
2019-05-22 17:54:25 +0200

    changed project

--- first_project_dima/first_script.R
+++
@@ -1,9 +0,0 @@
-matt <- GSE122020_series_matrix
-
-write.csv(matt, file='gse122020_matrix_IDasInd.csv',row.names = 1)
-read.csv('gse122020_matrix_IDasInd.csv')
-
-write.csv(matt, file='gse122020_matrix_IDasCol.csv')
-read.csv('gse122020_matrix_IDasCol.csv')
-
-
